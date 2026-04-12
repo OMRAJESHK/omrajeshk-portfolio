532cf7a4746628a552f2a9f85d60fd6e95e24601
Om Rajesh K <omrajeshk6021@gmail.com>
2026-04-12 15:14:05 +0530

    op-main | Rajesh | Update Next.js config for Vercel deployment

--- next.config.ts
+++ next.config.ts
@@ -1,10 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  output: 'export',  // Enables static export
-  trailingSlash: true,  // Required for static hosting
-  basePath: '/omrajeshk-portfolio',  // Replace with your GitHub repo name (e.g., '/your-repo-name')
-  assetPrefix: '/omrajeshk-portfolio/',  // Same as basePath, with trailing slash
+  /* config options here */
 };
 
 export default nextConfig;
